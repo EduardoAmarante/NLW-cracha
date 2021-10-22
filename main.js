8b9ae362ef809cda1e76c42fe52794a54f930324
Eduardo Amarante <eamarante80@gmail.com>
2021-10-21 23:00:25 -0300

    dia 4 do desafio

---
+++ main.js
@@ -0,0 +1,34 @@
+
+const link ={
+  github : "EduardoAmarante",
+  youtube : "droidhs",
+  facebook : "",
+  instagram : "eduardo4marante",
+  twitter : ""
+}
+
+function changeSocialMediaLinks(){
+  for(let li of socialLinks.children){
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${link[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos(){
+  const url = `https://api.github.com/users/${link.github}`
+
+  fetch(url)
+  .then(response => response.json() )
+  .then(data => {
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+    userPhoto.src = data.avatar_url
+    userGitHub.href = data.html_url
+    userLogin.textContent = data.login
+  })
+
+}
+
+getGitHubProfileInfos()
